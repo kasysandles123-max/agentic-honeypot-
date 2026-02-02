3b12c8917958f5df06559bbedc578c71f198aead
kasysandles123-max <kasysandles123@gmail.com>
2026-02-02 23:30:33 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+API_KEY = "sk_test_123456789"   # yahi key tester me daalna
+
+GUVI_CALLBACK_URL = "https://hackathon.guvi.in/api/updateHoneyPotFinalResult"
+
+SCAM_KEYWORDS = [
+    "account blocked", "verify", "urgent",
+    "upi", "bank", "click link", "suspended"
+]
